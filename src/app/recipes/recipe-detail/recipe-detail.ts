01e9edfb0ab5d2972c6b479489a23cce8dabe3dc
piotr-woz <piotrwozniak.dev@gmail.com>
2026-02-22 01:26:13 +0100

    feat(recipe-item): implement component with recipe data inputs

--- src/app/recipes/recipe-detail/recipe-detail.ts
+++ src/app/recipes/recipe-detail/recipe-detail.ts
@@ -2,7 +2,6 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-detail',
-  imports: [],
   templateUrl: './recipe-detail.html',
   styleUrl: './recipe-detail.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/recipes/recipe-list/recipe-item/recipe-item.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.ts
@@ -1,10 +1,17 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
-  imports: [],
   templateUrl: './recipe-item.html',
   styleUrl: './recipe-item.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'd-flex justify-content-between align-items-center',
+  },
 })
-export class RecipeItem {}
+export class RecipeItem {
+  public readonly recipe = input.required<Recipe>();
+}
+
+// click: 'onSelectedRecipe()',
